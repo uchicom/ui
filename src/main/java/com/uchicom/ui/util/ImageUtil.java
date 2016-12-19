33233ede8a32f368883158191304b7ba46969dff
shigeki <shigeki@wk2>
2016-12-20 03:43:15 +0900

    syoから移動

---
+++ src/main/java/com/uchicom/ui/util/ImageUtil.java
@@ -0,0 +1,15 @@
+// (c) 2016 uchicom
+package com.uchicom.ui.util;
+
+import javax.swing.ImageIcon;
+
+/**
+ * @author uchicom: Shigeki Uchiyama
+ *
+ */
+public class ImageUtil {
+
+	public static ImageIcon getImageIcon(String path) {
+		return new ImageIcon(Thread.currentThread().getContextClassLoader().getResource(path));
+	}
+}
