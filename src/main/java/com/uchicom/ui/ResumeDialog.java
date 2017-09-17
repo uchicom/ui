6b595c91f6863c498a9dc3091b3a004151f4fe5a
Shigeki Uchiyama <shigeki@wk2>
2017-09-17 11:38:06 +0900

    クラス階層を変更、Dialogも追加

--- src/main/java/com/uchicom/ui/ResumeDialog.java
+++ src/main/java/com/uchicom/ui/ResumeDialog.java
@@ -7,51 +7,45 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
+import java.util.Properties;
 
-import javax.swing.JFrame;
+import javax.swing.JDialog;
 
 /**
  * @author uchicom: Shigeki Uchiyama
  *
  */
-public class ResumeFrame extends ConfigFrame {
+public class ResumeDialog extends JDialog {
 
+	private Properties config;
 	private static final String PROP_SPLIT_CHAR = ":";
 	private static final String PROP_WINDOW_POSITION = "window.position";
-	private static final String PROP_WINDOW_STATE = "window.state";
-	public ResumeFrame(File configFile, String windowKey) {
-		super(configFile);
-		initComponents(windowKey);
+
+	public ResumeDialog(Properties config, String dialogKey) {
+		this.config = config;
+		initComponents(dialogKey);
 	}
+
 	private void initComponents(String windowKey) {
 		String positionKey = PROP_WINDOW_POSITION + "." + windowKey;
-		String stateKey = PROP_WINDOW_STATE + "." + windowKey;
 		setWindowPosition(this, positionKey);
-		setWindowState(this, stateKey);
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent we) {
-				if (ResumeFrame.this.getExtendedState() == JFrame.NORMAL) {
-					// 画面の位置を保持する
-					storeWindowPosition(ResumeFrame.this, positionKey);
-				}
-				storeWindowState(ResumeFrame.this, stateKey);
+				// 画面の位置を保持する
+				storeWindowPosition(ResumeDialog.this, positionKey);
 			}
 
 		});
 		addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentMoved(ComponentEvent ce) {
-				if (getExtendedState() == JFrame.NORMAL) {
-					storeWindowPosition(ResumeFrame.this, positionKey);
-				}
+				storeWindowPosition(ResumeDialog.this, positionKey);
 			}
+
 			@Override
 			public void componentResized(ComponentEvent ce) {
-				if (getExtendedState() == JFrame.NORMAL) {
-					storeWindowPosition(ResumeFrame.this, positionKey);
-				}
+				storeWindowPosition(ResumeDialog.this, positionKey);
 			}
 		});
 
@@ -66,17 +60,6 @@ public class ResumeFrame extends ConfigFrame {
 	private void storeWindowPosition(Window window, String key) {
 		String value = window.getLocation().x + PROP_SPLIT_CHAR + window.getLocation().y + PROP_SPLIT_CHAR
 				+ window.getWidth() + PROP_SPLIT_CHAR + window.getHeight() + PROP_SPLIT_CHAR;
-		config.setProperty(key, value);
-	}
-	/**
-	 * 画面の位置をプロパティに設定する。
-	 *
-	 * @param frame
-	 * @param key
-	 */
-	private void storeWindowState(JFrame frame, String key) {
-		String value = frame.getState() + PROP_SPLIT_CHAR
-				+ frame.getExtendedState();
 		config.setProperty(key, value);
 	}
 
@@ -96,16 +79,5 @@ public class ResumeFrame extends ConfigFrame {
 			}
 		}
 	}
-	public void setWindowState(JFrame frame, String key) {
-		if (config.containsKey(key)) {
-			String initPoint = config.getProperty(key);
-			String[] points = initPoint.split(PROP_SPLIT_CHAR);
-			if (points.length > 1) {
-				frame.setState(Integer.parseInt(points[0]));
-				frame.setExtendedState(Integer.parseInt(points[1]));
-			}
-		}
-	}
-
 
 }
