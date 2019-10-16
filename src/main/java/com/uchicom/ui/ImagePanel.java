59efe66aa27c4a03268b3d1f9ad2f0ae77fd612c
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2019-10-16 23:50:07 +0900

    背景黒塗りつぶしと中央表示

--- src/main/java/com/uchicom/ui/ImagePanel.java
+++ src/main/java/com/uchicom/ui/ImagePanel.java
@@ -1,7 +1,7 @@
 // (c) 2018 uchicom
 package com.uchicom.ui;
 
-
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -9,6 +9,8 @@ import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 /**
+ * 画像を描画するパネル.
+ * 
  * @author uchicom: Shigeki Uchiyama
  *
  */
@@ -24,8 +26,26 @@ public class ImagePanel extends JPanel {
 		this.image = image;
 		setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
 	}
+
+	@Override
+	public void update(Graphics g) {
+		paint(g);
+	}
+
 	@Override
 	public void paint(Graphics g) {
-		g.drawImage(image, 0, 0, this);
+		// 背景色を塗りつぶし
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		// 中央に表示
+		int x = 0, y = 0;
+		if (image.getWidth() < getWidth()) {
+			x = (getWidth() - image.getWidth()) / 2;
+		}
+		if (image.getHeight() < getHeight()) {
+			y = (getHeight() - image.getHeight()) / 2;
+		}
+		// 画像描画
+		g.drawImage(image, x, y, this);
 	}
 }
