ae72ded897b6c239415bccfbdefda399e0c366c2
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2021-02-17 14:37:56 +0900

    モジュール設定追加

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+// (c) 2021 uchicom
+module com.uchicom.ui {
+	requires java.desktop;
+	requires com.uchicom.util;
+	exports com.uchicom.ui;
+	exports com.uchicom.ui.action;
+	exports com.uchicom.ui.util;
+}
